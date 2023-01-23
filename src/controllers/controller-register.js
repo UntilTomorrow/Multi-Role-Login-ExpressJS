1301cfd13c80a1a9a4cecd26441831eef58732cf
UntilTomorrow <herucahyo55@gmail.com>
2023-01-23 19:53:58 +0700

    Commit 23-01-2023

---
+++ src/controllers/controller-register.js
@@ -0,0 +1,36 @@
+const config = require('../configs/database');
+let mysql = require('mysql');
+let pool = mysql.createPool(config);
+
+pool.on('error', (err)=> {
+    console.error(err);
+});
+
+module.exports ={
+    formRegister(req,res){
+        res.render("register",{
+            url : 'http://localhost:3000'
+        });
+    },
+    saveRegister(req,res){
+        let username = req.body.username;
+        let email = req.body.email;
+        let password = req.body.pass;
+        if (username && email && password) {
+            pool.getConnection(function(err, connection){
+                if(err) throw err;
+                connection.query(
+                    `INSERT INTO login_user(user_name,user_email,user_password) VALUES (?,?,SHA2(?,512));`
+                , [username, email, password], function (error, results) {
+                    if(error) throw error;
+                    req.flash('color', 'success');
+                    req.flash('status', 'Yes..');
+                    req.flash('message', 'Registrasi berhasil');
+                    res.redirect('/login');
+                });
+            })
+
+        }
+    }
+
+}
