ab6f8b927e923f884274cabfbc8a5c7bf0cd5dfa
UntilTomorrow <herucahyo55@gmail.com>
2023-02-11 12:47:23 +0700

    update

--- src/controllers/controller-login.js
+++ src/controllers/controller-login.js
@@ -1,58 +1,58 @@
 const config = require('../configs/database');
 
-let mysql   = require('mysql');
-let pool    = mysql.createPool(config);
+let mysql      = require('mysql');
+let pool       = mysql.createPool(config);
 
-pool.on('error', (err)=> {
+pool.on('error',(err)=> {
     console.error(err);
 });
 
 module.exports ={
     login(req,res){
-        res.render("login", {
+        res.render("login",{
             url : 'http://localhost:3000/',
             colorFlash: req.flash('color'),
             statusFlash: req.flash('status'),
             pesanFlash: req.flash('message'),
         });
     },
-
     loginAuth(req,res){
         let email = req.body.email;
         let password = req.body.pass;
-        if (email && password ) {
-            pool.getConnection(function(err, connection){
+        if (email && password) {
+            pool.getConnection(function(err, connection) {
                 if (err) throw err;
                 connection.query(
                     `SELECT * FROM login_user WHERE user_email = ? AND user_password = SHA2(?,512)`
-                    , [email, password], function (error, results) {
-                        if (error) throw error;
-                        if (results.length > 0) {
-                            let sql = 'INSERT INTO loglogin set user_email = ?, login_time = ?';
-                            let values = [email, new Date()];
-                            connection.query (sql, values, function(){
-                            })
-                            req.session.longgedin = true;
-                            req.session.user = {
-                                        role: results[0].user_role
-                            };
-                            req.session.userid = results[0].user_id;
-                            req.session.userid = results[0].user_name;
-                            res.redirect('/')
-                        } else {
-                            req.flash('color','danger');
-                            req.flash('status', 'Ooops...');
-                            req.flash('message', 'Akun Tidak Ditemukan...');
-                            req.redirect('/login');
-                        }
-                    });
-                    connection.release();
+                , [email, password],function (error, results) {
+                    if (error) throw error;  
+                    if (results.length > 0) {
+                        let sql = 'INSERT INTO loglogin SET user_email = ?, login_time = ?';
+                        let values = [email, new Date()];
+                        connection.query (sql, values, function(){
+                        })
+                        req.session.loggedin = true;
+                        req.session.user = {
+                                role: results[0].user_role
+                        };
+                        console.log(req.session.user.role );
+                        req.session.userid = results[0].user_id;
+                        req.session.username = results[0].user_name;
+                        console.log(req.session.user)
+                        res.redirect('/');  
+                    } else {
+                        req.flash('color', 'danger');
+                        req.flash('status', 'Oops..');
+                        req.flash('message', 'Akun tidak ditemukan');
+                        res.redirect('/login');
+                    }
+                });
+                connection.release();
             })
-        }  else {
+        } else {
             res.redirect('/login');
             res.end();
         }
-
     },
     logout(req,res){
         req.session.destroy((err) => {
@@ -63,4 +63,6 @@ module.exports ={
             res.redirect('/login');
         });
     },
+
+    
 }
--- src/controllers/controller-register.js
+++ src/controllers/controller-register.js
@@ -1,39 +1,38 @@
 const config = require('../configs/database');
-let mysql = require('mysql');
-let pool = mysql.createPool(config);
+let mysql      = require('mysql');
+let pool       = mysql.createPool(config);
 
-pool.on('error', (err)=> {
+pool.on('error',(err)=> {
     console.error(err);
 });
 
 module.exports ={
     formRegister(req,res){
         res.render("register",{
-            url : 'http://localhost:3000'
+            url : 'http://localhost:3000/',
         });
     },
     saveRegister(req,res){
-        console.log(req.body);
         let username = req.body.username;
         let email = req.body.email;
         let password = req.body.pass;
-        let UID = req.body.UID;
-        if (username && email && password && UID) {
-            pool.getConnection(function(err, connection){
-                if(err) throw err;
+        if (username && email && password) {
+            pool.getConnection(function(err, connection) {
+                if (err) throw err;
                 connection.query(
-                    `INSERT INTO login_user(user_name, user_email, user_password, UID) VALUES (?,?,SHA2(?,512),SHA2(?,512));`,
-                    [username, email, password, UID], function (error, results) {
-                    if(error) throw error;
+                    `INSERT INTO login_user (user_name,user_email,user_password) VALUES (?,?,SHA2(?,512));`
+                , [username, email, password],function (error, results) {
+                    if (error) throw error; 
                     req.flash('color', 'success');
                     req.flash('status', 'Yes..');
                     req.flash('message', 'Registrasi berhasil');
                     res.redirect('/login');
                 });
+                connection.release();
             })
-        }else {
+        } else {
             res.redirect('/login');
+            res.end();
         }
     }
-
 }
