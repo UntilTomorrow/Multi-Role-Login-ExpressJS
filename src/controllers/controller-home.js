8f46187f57cf1c18e5356c20e2666801c08edc3d
UntilTomorrow <herucahyo55@gmail.com>
2023-02-11 12:54:49 +0700

    update

--- src/controllers/controller-home.js
+++ src/controllers/controller-home.js
@@ -2,7 +2,8 @@ module.exports = {
     home(req,res){
         res.render('home', {
             url: 'http//localhost:3000/',
-            userName: req.session.username
+            userName: req.session.username,
+            user: req.session.user,
         })
     }
 }
