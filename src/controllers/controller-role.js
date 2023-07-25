8bee9c5a7f54af7a48a04f1bb73ec2ee9af63a71
UntilTomorrow <herucahyo55@gmail.com>
2023-07-25 22:54:55 +0700

    update

--- src/controllers/controller-role.js
+++ src/controllers/controller-role.js
@@ -7,3 +7,25 @@ pool.on('error', (err) => {
     console.error(err);
 });
 
+module.exports = {
+    role(req, res) {
+      // Pastikan pengguna sudah login
+      if (req.session.loggedin !== true) {
+        res.redirect('/login');
+        return;
+      }
+  
+      // Pastikan pengguna memiliki role "admin"
+      if (req.session.role === 'admin') {
+        // Jika pengguna adalah admin, lanjutkan eksekusi controller dan render halaman "role"
+        res.render('role', {
+          url: 'http://localhost:3000/',
+          userName: req.session.username,
+          user: req.session.user,
+        });
+      } else {
+        // Jika pengguna bukan admin, beri respons dengan status 403 Forbidden
+        res.status(403).send('Forbidden');
+      }
+    }
+  }
