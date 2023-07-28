e16cc0be50977d2222714f37f4f62723746a301f
UntilTomorrow <herucahyo55@gmail.com>
2023-07-28 20:22:30 +0700

    update index

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,12 @@ const login = require('./controller-login');
 const register = require('./controller-register');
 const home = require('./controller-home');
 const role = require('./controller-role');
+const profile = require('./controller-profile');
 
 module.exports = {
     login,
     register,
     home,
-    role
+    role,
+    profile
 };
