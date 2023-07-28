90ecb80607e3feacbf8f49283ddaa1b1c0ba8212
UntilTomorrow <herucahyo55@gmail.com>
2023-07-28 20:22:37 +0700

    route profile

--- src/routes/router-profile.js
+++ src/routes/router-profile.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const loginController = require('../controllers').login;
+const profileController = require('../controllers').profile;
 const verifyUser = require('../configs/verify');
 
 router.get('/', verifyUser.isLogin, profileController.profile);
